5dcf65bfbe07bb21f0503ca2479f406d8037a8e5
wopgan <sylviolimas@gmail.com>
2023-05-06 13:32:25 -0300

    adicionando titulo obra e area upload video keyworkdVideo

--- backend/ida_dialog/models.py
+++ backend/ida_dialog/models.py
@@ -6,6 +6,8 @@ class listDiolog(models.Model):
         verbose_name = "IdaQr Dado"
         verbose_name_plural = "IdaQr Dados"
 
+    keyTitle = models.CharField(max_length=100, verbose_name='Titulo da Obra')
+    keywordVideo = models.FileField(upload_to='static/', verbose_name='Arquivo de video')
     keywordName = models.CharField(max_length=50, verbose_name='Comando')
     keywordNarrative = models.FileField(upload_to='static/', verbose_name='Arquivo de audio OGG')
     keywordSpeech = models.TextField(verbose_name='Instruções de Sequência')
--- backend/ida_dialog/serializers.py
+++ backend/ida_dialog/serializers.py
@@ -4,4 +4,4 @@ from .models import listDiolog
 class IdaDialogSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = listDiolog
-        fields = ['id', 'keywordName', 'keywordNarrative', 'keywordSpeech']
+        fields = ['id', 'keywordName', 'keyTitle', 'keywordVideo' ,'keywordNarrative', 'keywordSpeech']
